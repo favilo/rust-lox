e310bf45db2b9e9fcfac7a87f18bf2df25884bb7
favilo <kevin.oberlies@elastic.co>
2025-02-28 14:48:47 -0800

    feat: Scanning: other single-character tokens

--- src/token.rs
+++ src/token.rs
@@ -3,6 +3,7 @@ use std::fmt;
 use winnow::{
     ascii::multispace0,
     combinator::{alt, eof, repeat, terminated},
+    error::Result,
     Parser,
 };
 
@@ -12,7 +13,34 @@ pub enum Token {
     RightParen,
     LeftBrace,
     RightBrace,
+
+    Dot,
+    Comma,
+    Minus,
+    Plus,
+    Star,
+    Slash,
+    Semicolon,
     Eof,
+}
+
+impl Token {
+    fn parser(input: &mut &str) -> Result<Self> {
+        alt((
+            "(".map(|_| Token::LeftParen),
+            ")".map(|_| Token::RightParen),
+            "{".map(|_| Token::LeftBrace),
+            "}".map(|_| Token::RightBrace),
+            ".".map(|_| Token::Dot),
+            ",".map(|_| Token::Comma),
+            "-".map(|_| Token::Minus),
+            "+".map(|_| Token::Plus),
+            "*".map(|_| Token::Star),
+            "/".map(|_| Token::Slash),
+            ";".map(|_| Token::Semicolon),
+        ))
+        .parse_next(input)
+    }
 }
 
 impl fmt::Display for Token {
@@ -22,27 +50,21 @@ impl fmt::Display for Token {
             Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
             Token::LeftBrace => write!(f, "LEFT_BRACE {{ null"),
             Token::RightBrace => write!(f, "RIGHT_BRACE }} null"),
+            Token::Dot => write!(f, "DOT . null"),
+            Token::Comma => write!(f, "COMMA , null"),
+            Token::Minus => write!(f, "MINUS - null"),
+            Token::Plus => write!(f, "PLUS + null"),
+            Token::Star => write!(f, "STAR * null"),
+            Token::Slash => write!(f, "SLASH / null"),
+            Token::Semicolon => write!(f, "SEMICOLON ; null"),
+
             Token::Eof => write!(f, "EOF  null"),
         }
     }
 }
 
-pub fn tokenize(input: &mut &str) -> winnow::error::Result<Vec<Token>> {
-    terminated(
-        repeat(
-            0..,
-            terminated(
-                alt((
-                    "(".map(|_| Token::LeftParen),
-                    ")".map(|_| Token::RightParen),
-                    "{".map(|_| Token::LeftBrace),
-                    "}".map(|_| Token::RightBrace),
-                )),
-                multispace0,
-            ),
-        ),
-        eof,
-    )
-    .map(|tokens: Vec<_>| tokens.into_iter().chain([Token::Eof]).collect::<Vec<_>>())
-    .parse_next(input)
+pub fn tokenize(input: &mut &str) -> Result<Vec<Token>> {
+    terminated(repeat(0.., terminated(Token::parser, multispace0)), eof)
+        .map(|tokens: Vec<_>| tokens.into_iter().chain([Token::Eof]).collect::<Vec<_>>())
+        .parse_next(input)
 }
